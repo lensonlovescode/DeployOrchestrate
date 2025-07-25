eacfb8fb8c764b9902759aebf1c0e8052b12d36a
lensonlovescode <lensonisinparis@gmail.com>
2025-07-26 01:27:46 +0300

    Puppet manifests - setup and application

---
+++ applyManifests.pp
@@ -0,0 +1,6 @@
+# This puppet manifest Applies all yaml manifests to kubernetes minikube
+
+exec {'apply manifests':
+  command => 'kubectl apply -f /MinikubeManifests',
+  path => ['/usr/bin', '/bin, '/usr/local/bin'],
+}
---
+++ serverSetup.pp
@@ -0,0 +1,53 @@
+# This puppet manifest:
+#  1. updates the server
+#  2. Installs and runs nginx
+#  3. Installs and runs Docker
+#  4. Ensures Minikube is installed
+#  5. Ensures Minikube is running with one control node and one worker node
+
+exec { 'update':
+  command  => '/usr/bin/apt-get update',
+  provider => 'shell'
+}
+
+package { 'nginx':
+  ensure  => 'installed',
+  require => Exec['update']
+}
+
+service {'nginx':
+  ensure => running,
+  enable => true,
+  require => Package['nginx']
+}
+
+package {'docker-ce':
+  ensure => 'installed',
+  require => service['nginx']
+}
+
+service {'docker':
+  ensure => running,
+  enable => true,
+  require => Package['docker']
+}
+
+exec {'install minikube':
+  command => 'curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64 && install minikube-linux-amd64 /usr/local/bin/minikube',
+  unless => 'which minikube',
+  path => ['/usr/bin', '/bin', '/usr/local/bin']
+}
+
+
+exec {'install kubectl':
+  command => 'curl -LO https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl &&
+             install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl && rm kubectl',
+  unless => 'command -v kubectl',
+  path => ['/usr/bin', '/bin', '/usr/local/bin'],
+}
+
+exec {'start cluster':
+  command => 'minikube start',
+  unless => 'minikube status | grep -q Running',
+  path => ['/usr/bin', '/bin', '/usr/local/bin'],
+}
